fa702dc8e411d85ea77447d907b40bbb90ff2694
Reza Ghahremani <developer.ghahremani@gmail.com>
2022-05-18 09:13:11 +0430

    firstly init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './app/auth/auth.module';
+import { Module } from '@nestjs/common';
+import { UserModule } from './app/user/user.module';
+
+@Module({
+  imports: [UserModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
