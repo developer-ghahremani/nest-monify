33aca39542abbd995cb9aa16f64c923f57bbee64
Reza Ghahremani <developer.ghahremani@gmail.com>
2022-05-21 18:49:01 +0430

    fix cors policy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,27 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './app/auth/auth.module';
+import { CategoryModule } from './app/category/category.module';
 import { ConfigModule } from '@nestjs/config';
 import { FinancialUnitModule } from './app/financial-unit/financial-unit.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './app/user/user.module';
-import { WalletModule } from './app/wallet/wallet.module';
 import { SourceModule } from './app/source/source.module';
-import { CategoryModule } from './app/category/category.module';
 import { TagModule } from './app/tag/tag.module';
 import { TransactionModule } from './app/transaction/transaction.module';
+import { UserModule } from './app/user/user.module';
+import { WalletModule } from './app/wallet/wallet.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      `mongodb://localhost:27017/monify-${process.env.NODE_ENV.trim()}?authSource=admin`,
-      // `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${
-      //   process.env.DB_HOST
-      // }:${
-      //   process.env.DB_PORT
-      // }/monify-${process.env.NODE_ENV.trim()}?authSource=admin`,
+      // `mongodb://localhost:27017/monify-${process.env.NODE_ENV.trim()}?authSource=admin`,
+      `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${
+        process.env.DB_HOST
+      }:${
+        process.env.DB_PORT
+      }/monify-${process.env.NODE_ENV.trim()}?authSource=admin`,
     ),
     AuthModule,
     UserModule,
